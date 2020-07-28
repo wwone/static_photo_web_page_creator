e32f715554a102e9391f7c8a7d0d03724c0c77af
Bob Swanson <40324245+wwone@users.noreply.github.com>
2020-07-28 11:19:37 -0500

    Update compit.sh
    Correct filename by copying content of previous script version

--- compit.sh
+++ compit.sh
@@ -1,1 +1,30 @@
 # Linux shell script that compiles the Java code
+#
+# this is a Linux shell script
+#
+# it compiles the Java program for the static HTML photo page creator
+#
+# This code IS DEPENDENT on the use of 'Jackson' for JSON processing
+#
+
+
+
+JAVAC=javac
+echo "- - - - - - -"
+echo "- - - - - - -"
+echo "- - - - - - -"
+echo "- - - - - - -"
+
+
+# following for JSON
+CP=$CP:jackson-core-2.9.7.jar
+CP=$CP:jackson-databind-2.9.7.jar
+CP=$CP:.
+
+OBJ=MakeWeb
+
+
+
+echo "$JAVAC" -classpath "$CP" $OBJ.java
+"$JAVAC" -classpath "$CP" $OBJ.java
+
